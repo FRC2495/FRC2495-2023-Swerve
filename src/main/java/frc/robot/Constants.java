cea135a70aed2257c385d14df75f5beb7e11fca8
frc2495 <frc2495@gmail.com>
2023-07-26 20:45:47 -0400

    tuned P for turns

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -110,7 +110,7 @@ public final class Constants {
     public static final double kDrivingMinOutput = -1;
     public static final double kDrivingMaxOutput = 1;
 
-    public static final double kTurningP = 1;
+    public static final double kTurningP = 1.0/100.0;
     public static final double kTurningI = 0;
     public static final double kTurningD = 0;
     public static final double kTurningFF = 0;
