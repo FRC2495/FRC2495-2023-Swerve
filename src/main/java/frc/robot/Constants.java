14268ed6da77e6664f69c32094eca38d3d5e2e30
frc2495 <frc2495@gmail.com>
2023-08-04 15:16:40 -0400

    reduced slew rates

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -31,8 +31,8 @@ public final class Constants {
     public static final double kMaxAngularSpeed = 2 * Math.PI; // radians per second
 
     public static final double kDirectionSlewRate = 1.2; // radians per second
-    public static final double kMagnitudeSlewRate = 0.8; // percent per second (1 = 100%)
-    public static final double kRotationalSlewRate = 0.8; // percent per second (1 = 100%)
+    public static final double kMagnitudeSlewRate = 2; // percent per second (1 = 100%)
+    public static final double kRotationalSlewRate = 2; // percent per second (1 = 100%)
 
     // Chassis configuration
     public static final double kTrackWidth = Units.inchesToMeters(21.75);
