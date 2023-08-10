20499e532c677f9eea17f0c44846c96129bb1de5
Mr. A <GabrielArtaud@comcast.net>
2023-08-10 14:33:49 -0400

    set all angular offsets to zero for now,  may remove completely later

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -36,8 +36,10 @@ public final class Constants {
 
 		// Chassis configuration
 		public static final double kTrackWidth = Units.inchesToMeters(21.75);
+		
 		// Distance between centers of right and left wheels on robot
 		public static final double kWheelBase = Units.inchesToMeters(21.75);
+		
 		// Distance between front and back wheels on robot
 		public static final SwerveDriveKinematics kDriveKinematics = new SwerveDriveKinematics(
 				new Translation2d(kWheelBase / 2, kTrackWidth / 2),
@@ -46,10 +48,10 @@ public final class Constants {
 				new Translation2d(-kWheelBase / 2, -kTrackWidth / 2));
 
 		// Angular offsets of the modules relative to the chassis in radians
-		public static final double kFrontLeftChassisAngularOffset = -Math.PI / 2;
-		public static final double kFrontRightChassisAngularOffset = 0;
-		public static final double kBackLeftChassisAngularOffset = Math.PI;
-		public static final double kBackRightChassisAngularOffset = Math.PI / 2;
+		public static final double kFrontLeftChassisAngularOffset = 0.0; // -Math.PI / 2;
+		public static final double kFrontRightChassisAngularOffset = 0.0; // 0;
+		public static final double kBackLeftChassisAngularOffset = 0.0; // Math.PI;
+		public static final double kBackRightChassisAngularOffset = 0.0; // Math.PI / 2;
 
 		// SPARK MAX CAN IDs
 		public static final int kFrontLeftDrivingCanId = 8;
