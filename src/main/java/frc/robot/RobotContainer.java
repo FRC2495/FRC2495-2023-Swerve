e0ecee8a3bb5f114d9213deb09edae4e91ab19b4
Mr. A <GabrielArtaud@comcast.net>
2023-08-12 11:58:44 -0400

    switched to CommandXboxController

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -13,20 +13,25 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.TrajectoryConfig;
 import edu.wpi.first.math.trajectory.TrajectoryGenerator;
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.XboxController.Button;
+//import edu.wpi.first.wpilibj.XboxController;
+//import edu.wpi.first.wpilibj.XboxController.Button;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController; 
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
+//import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+//import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import java.util.List;
+
 import frc.robot.Constants.AutoConstants;
 import frc.robot.Constants.DrivetrainConstants;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Indicator;
 import frc.robot.commands.indicator.*;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import edu.wpi.first.wpilibj.smartdashboard.Field2d;
-import java.util.List;
+
 
 /*
  * This class is where the bulk of the robot should be declared.  Since Command-based is a
@@ -38,15 +43,77 @@ public class RobotContainer {
 
 	public static final double GAMEPAD_AXIS_THRESHOLD = 0.05;
 
-	private final Field2d field = new Field2d(); //  a representation of the field
+	// choosers (for auton)
+	
+	public static final String AUTON_DO_NOTHING = "Do Nothing";
+	public static final String AUTON_CUSTOM = "My Auto";
+	private String autonSelected;
+	private SendableChooser<String> autonChooser = new SendableChooser<>();
 
-	// The robot's subsystems
+	public static final String GAME_PIECE_NONE = "None";
+	public static final String GAME_PIECE_1_CONE = "1 Cone";
+	public static final String GAME_PIECE_2_CONES = "2 Cones";
+	private String gamePieceSelected;
+	private SendableChooser<String> gamePieceChooser = new SendableChooser<>();
+	
+	public static final String START_POSITION_1 = "Starting Position 1";
+	public static final String START_POSITION_2 = "Starting Position 2";
+	public static final String START_POSITION_3 = "Starting Position 3";
+	public static final String START_POSITION_4 = "Starting Position 4";
+	public static final String START_POSITION_5 = "Starting Position 5";
+	public static final String START_POSITION_6 = "Starting Position 6";
+	private String startPosition;
+	private SendableChooser<String> startPositionChooser = new SendableChooser<>();
+
+	public static final String MAIN_TARGET_CONE_NODE = "Cone Node";
+	public static final String MAIN_TARGET_TWO_CONE_NODES = "Two Cone Nodes";
+	public static final String MAIN_TARGET_CHARGING_STATION = "Charging Station";
+	public static final String MAIN_TARGET_NOWHERE = "Nowhere";
+	private String mainTarget;
+	private SendableChooser<String> mainTargetChooser = new SendableChooser<>();
+	
+	public static final String CAMERA_OPTION_USE_ALWAYS = "Always";
+	public static final String CAMERA_OPTION_USE_OPEN_LOOP_ONLY = "Open Loop Only";
+	public static final String CAMERA_OPTION_USE_CLOSED_LOOP_ONLY = "Closed Loop Only";
+	public static final String CAMERA_OPTION_USE_NEVER = "Never";
+	private String cameraOption;
+	private SendableChooser<String> cameraOptionChooser = new SendableChooser<>();
+	
+	public static final String SONAR_OPTION_USE_ALWAYS = "Always";
+	public static final String SONAR_OPTION_USE_RELEASE_ONLY = "Release Only";
+	public static final String SONAR_OPTION_USE_GRASP_ONLY = "Grasp Only";
+	public static final String SONAR_OPTION_USE_NEVER = "Never";
+	private String sonarOption;
+	private SendableChooser<String> sonarOptionChooser = new SendableChooser<>();
+	
+	public static final String CLAW_OPTION_RELEASE = "Release";
+	public static final String CLAW_OPTION_DONT_RELEASE = "Don't Release"; 
+	private String releaseSelected;
+	private SendableChooser<String> releaseChooser = new SendableChooser<>();
+
+	public static final String AUTON_OPTION_JUST_DROP_CONE = "Just Drop Cone";
+	public static final String AUTON_OPTION_ALSO_DOCK = "Also Dock";
+	public static final String AUTON_OPTION_LEAVE_COMMUNITY = "Leave Community";
+	public static final String AUTON_OPTION_ALSO_PICKUP_CONE = "Also Pickup Cone";
+	private String autonOption;
+	private SendableChooser<String> autonOptionChooser = new SendableChooser<>();
+
+	// sensors
+
+	// motorized devices
+
 	private final Drivetrain m_robotDrive = new Drivetrain();
+
+	// pneumatic devices
+
+	// misc
+
+	private final Field2d field = new Field2d(); //  a representation of the field
 
 	private final Indicator indicator = new Indicator(null);
 
 	// The driver's controller
-	XboxController m_driverController = new XboxController(Ports.USB.GAMEPAD);
+	CommandXboxController m_driverController = new CommandXboxController(Ports.USB.GAMEPAD);
 
 	/**
 	 * The container for the robot. Contains subsystems, OI devices, and commands.
@@ -83,17 +150,17 @@ public class RobotContainer {
 	 * {@link JoystickButton}.
 	 */
 	private void configureButtonBindings() {
-		new JoystickButton(m_driverController, Button.kX.value)
+		m_driverController.x()
 			.whileTrue(new RunCommand(
 				() -> m_robotDrive.setX(),
 				m_robotDrive));
 
-		new JoystickButton(m_driverController, Button.kY.value)
+		m_driverController.y()
 			.onTrue(new InstantCommand(
 				() -> m_robotDrive.resetEncoders(),
 				m_robotDrive).ignoringDisable(true));
 
-		new JoystickButton(m_driverController, Button.kA.value)
+		m_driverController.a()
 			.onTrue(new InstantCommand(
 				() -> m_robotDrive.zeroHeading(),
 				m_robotDrive).ignoringDisable(true));      
