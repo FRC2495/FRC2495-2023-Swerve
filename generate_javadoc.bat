6cf252327fb1e1f1b780ba089f60bee3ab17c82e
Mr. A <GabrielArtaud@comcast.net>
2023-08-11 23:23:05 -0400

    updated batch file to generate javadoc

--- generate_javadoc.bat
+++ generate_javadoc.bat
@@ -1,1 +1,1 @@
- C:\Users\Public\wpilib\2023\jdk\bin\javadoc -sourcepath .\src\main\java\ -subpackages frc.robot  -classpath .\build\libs\MAXSWERVE.jar -d c:\temp\doc\MAXSWERVE
+ C:\Users\Public\wpilib\2023\jdk\bin\javadoc -sourcepath .\src\main\java\ -subpackages frc.robot  -classpath .\build\libs\FRC2495-2023-SWERVE.jar -d c:\temp\doc\FRC2495-2023-SWERVE
