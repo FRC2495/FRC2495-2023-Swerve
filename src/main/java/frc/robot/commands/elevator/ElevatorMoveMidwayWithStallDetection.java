770241a0cca37f9006d33b8123544c2ae1a98c9e
frc2495 <frc2495@gmail.com>
2023-10-20 19:11:29 -0400

    added elevatormovemidway command

---
+++ src/main/java/frc/robot/commands/elevator/ElevatorMoveMidwayWithStallDetection.java
@@ -0,0 +1,45 @@
+
+package frc.robot.commands.elevator;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+import frc.robot.subsystems.Elevator;
+
+/**
+ *
+ */
+/*public*/ class ElevatorMoveMidwayWithStallDetection extends CommandBase {
+
+	private Elevator elevator;
+
+	public ElevatorMoveMidwayWithStallDetection(Elevator elevator) {	
+		this.elevator = elevator;
+		addRequirements(elevator);
+	}
+
+	// Called just before this Command runs the first time
+	@Override
+	public void initialize() {
+		System.out.println("ElevatorMoveMidwayWithStallDetection initialize");
+		elevator.moveMidway();
+	}
+
+	// Called repeatedly when this Command is scheduled to run
+	@Override
+	public void execute() {
+		// nothing
+	}
+
+	// Make this return true when this Command no longer needs to run execute()
+	@Override
+	public boolean isFinished() {
+		return !elevator.tripleCheckMove() || elevator.tripleCheckIfStalled();
+	}
+
+	// Called once after isFinished returns true
+	@Override
+	public void end(boolean interrupted) {
+		System.out.println("ElevatorExtendMidwayWithStallDetection: end");
+		elevator.stop(); // adjust if needed
+	}
+}
