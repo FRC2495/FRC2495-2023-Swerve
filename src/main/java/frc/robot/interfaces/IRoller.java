d4b550b546196424c6416052258ca3b6f61327e0
frc2495 <frc2495@gmail.com>
2023-10-20 17:32:23 -0400

    added roller files from grasper 2022

---
+++ src/main/java/frc/robot/interfaces/IRoller.java
@@ -0,0 +1,25 @@
+package frc.robot.interfaces;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+
+public interface IRoller extends Subsystem {
+
+	public void roll();
+	
+	public void release();
+	
+	public void stop();
+		
+	// NOTE THAT THIS METHOD WILL IMPACT BOTH OPEN AND CLOSED LOOP MODES
+	public void setNominalAndPeakOutputs(double peakOutput);
+	
+	public boolean isRolling();
+	
+	public boolean isReleasing();
+
+	// for debug purpose only
+	public void joystickControl(Joystick joystick);
+
+}
