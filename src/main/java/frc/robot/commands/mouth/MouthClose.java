37bb61ce988e75f22ea7bd2fe0aa083754b45cd4
FRC2495 <frc2495@gmail.com>
2023-10-20 16:46:55 -0700

    added mouthopen and mouthclose command

---
+++ src/main/java/frc/robot/commands/mouth/MouthClose.java
@@ -0,0 +1,26 @@
+package frc.robot.commands.mouth;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+import frc.robot.subsystems.Mouth;
+import frc.robot.subsystems.Mouth.Position;
+
+// To set mouth open
+public class MouthClose extends CommandBase {
+
+	
+	private Mouth mouth;
+
+	public MouthClose(Mouth mouth) {	
+		this.mouth = mouth;
+		addRequirements(mouth);
+	}
+
+	// Called once when the command executes
+	@Override
+	public void initialize() {
+		System.out.println("MouthSetClose: initialize");
+		mouth.setPosition(Position.CLOSED);
+	}
+
+}
---
+++ src/main/java/frc/robot/commands/mouth/MouthOpen.java
@@ -0,0 +1,26 @@
+package frc.robot.commands.mouth;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+import frc.robot.subsystems.Mouth;
+import frc.robot.subsystems.Mouth.Position;
+
+// To set mouth open
+public class MouthOpen extends CommandBase {
+
+	
+	private Mouth mouth;
+
+	public MouthOpen(Mouth mouth) {	
+		this.mouth = mouth;
+		addRequirements(mouth);
+	}
+
+	// Called once when the command executes
+	@Override
+	public void initialize() {
+		System.out.println("MouthSetOpen: initialize");
+		mouth.setPosition(Position.OPEN);
+	}
+
+}
