74f7e23f2e175cb118d768613894885e3932a923
frc2495 <frc2495@gmail.com>
2023-11-04 11:06:32 -0400

    added move forward auton

--- src/main/java/frc/robot/auton/common/MoveForward.java
+++ src/main/java/frc/robot/auton/common/MoveForward.java
@@ -2,9 +2,12 @@ package frc.robot.auton.common;
 
 import java.util.List;
 
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
 import edu.wpi.first.math.trajectory.TrajectoryGenerator;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
@@ -14,13 +17,13 @@ import frc.robot.auton.AutonConstants;
 
 // GP = game piece
 // Can be used to place one cube or one cone and either starting position one or two
-public class CompletelyLeaveCommunity extends SequentialCommandGroup {
+public class MoveForward extends SequentialCommandGroup {
 
-    public CompletelyLeaveCommunity(RobotContainer container){
+    public MoveForward(RobotContainer container){
 
         addCommands(
 
-            container.createSwerveControllerCommand(createLeaveCommunityTrajectory(container))
+            container.createSwerveControllerCommand(createMoveForwardTrajectory(container))
 
             
         ); 
@@ -28,7 +31,7 @@ public class CompletelyLeaveCommunity extends SequentialCommandGroup {
     }
 
     
-    public Trajectory createLeaveCommunityTrajectory(RobotContainer container) {
+    public Trajectory createMoveForwardTrajectory(RobotContainer container) {
 		// An example trajectory to follow. All units in meters.
 		Trajectory trajectory = TrajectoryGenerator.generateTrajectory(
 			// Start at the origin facing the -X direction
@@ -37,7 +40,7 @@ public class CompletelyLeaveCommunity extends SequentialCommandGroup {
 			List.of(),
 			// End straight ahead of where we started, facing forward
 			new Pose2d(AutonConstants.DISTANCE_FROM_NODE_TO_OUTSIDE_COMMUNITY_METERS, 0, Rotation2d.fromDegrees(180)),
-			container.createReverseTrajectoryConfig());
+			container.createTrajectoryConfig());
 
 		return trajectory;
 	}
