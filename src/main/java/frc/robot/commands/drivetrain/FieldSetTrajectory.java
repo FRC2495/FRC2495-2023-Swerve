ac985db30221e5466831b8b41da01d098759c56c
Mr. A <GabrielArtaud@comcast.net>
2023-11-05 12:26:00 -0500

    added FieldSetTrajectory command

---
+++ src/main/java/frc/robot/commands/drivetrain/FieldSetTrajectory.java
@@ -0,0 +1,38 @@
+
+package frc.robot.commands.drivetrain;
+
+import edu.wpi.first.math.trajectory.Trajectory;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+
+import frc.robot.RobotContainer;
+
+/**
+ * Resets odometry at the specified pose
+ */
+public class FieldSetTrajectory extends InstantCommand {
+
+	private RobotContainer	container;
+	private Trajectory trajectory;
+
+	public FieldSetTrajectory(RobotContainer container, Trajectory trajectory) {
+		this.container = container;
+		this.trajectory = trajectory;
+
+		//addRequirements(); // nothing to reserve
+	}
+
+	// This instant command can run disabled
+	@Override
+	public boolean runsWhenDisabled() {
+		return true;
+	}
+
+	// Called once when this command runs
+	@Override
+	public void initialize() {
+		System.out.println("FieldSetTrajectory: initialize");
+		container.getField().getObject("trajectory").setTrajectory(trajectory);
+	}
+
+}
