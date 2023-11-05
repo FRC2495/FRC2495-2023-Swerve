4fa82912764c1d02dfc3f4d1182a28a7f2e321de
Mr. A <GabrielArtaud@comcast.net>
2023-11-05 11:42:13 -0500

    added DrivetrainFollowTrajectory command

---
+++ src/main/java/frc/robot/commands/drivetrain/DrivetrainFollowTrajectory.java
@@ -0,0 +1,46 @@
+
+package frc.robot.commands.drivetrain;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.Trajectory;
+
+import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
+
+import frc.robot.Constants.AutoConstants;
+import frc.robot.Constants.DrivetrainConstants;
+import frc.robot.subsystems.SwerveDrivetrain;
+
+/**
+ *
+ */
+public class DrivetrainFollowTrajectory extends SwerveControllerCommand {
+
+	//private SwerveDrivetrain drivetrain;
+
+	public DrivetrainFollowTrajectory(SwerveDrivetrain drivetrain, Trajectory trajectory, ProfiledPIDController thetaController) {
+		super(
+			trajectory, // trajectory to follow
+			drivetrain::getPose, // Functional interface to feed supplier
+			DrivetrainConstants.DRIVE_KINEMATICS, // kinematics of the drivetrain
+			new PIDController(AutoConstants.X_CONTROLLER_P, 0, 0), // trajectory tracker PID controller for x position
+			new PIDController(AutoConstants.Y_CONTROLLER_P, 0, 0), // trajectory tracker PID controller for y position
+			thetaController, // trajectory tracker PID controller for rotation
+			drivetrain::setModuleStates, // raw output module states from the position controllers
+			drivetrain); // subsystems to require
+	}
+
+	// Called just before this Command runs the first time
+	@Override
+	public void initialize() {
+		System.out.println("DrivetrainFollowTrajectory: initialize");
+		super.initialize();
+	}
+
+	// Called once after isFinished returns true
+	@Override
+	public void end(boolean interrupted) {
+		System.out.println("DrivetrainFollowTrajectory: end");
+		super.end(interrupted);
+	}
+}
